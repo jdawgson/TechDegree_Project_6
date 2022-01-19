16322a8944017fc7c4459f096f85c3fbde0ccb59
jdawgson <71034680+jdawgson@users.noreply.github.com>
2022-01-19 10:19:45 -0600

    finished project

--- js/app.js
+++ js/app.js
@@ -1,32 +1,74 @@
+// Phrases for game
 const phrasesList = ["How are you this evening",
                     "Treat others the way you want to be treated",
                     "Two wrongs dont make a right",
                     "Comparison is the theif of joy",
-                    "Shoot for the stars"];
+                    "Shoot for the stars" ];
+
 
 let phrase = "";
+let lives = 5;
+let phraseLength = "";
 
-$('#start-btn').click(() => {
-  $('#overlay').css('display', 'none');
+
+// Listens for click onn start buttonn and calls getPhrase and displays phrase
+$('#start-btn, #restart-btn-win, #restart-btn-loss').click(() => {
+  $('#overlay, .win, .loss').css('display', 'none');
   $('#banner, #phrase, #qwerty, #scoreboard').css('display', 'initial');
   let phraseSplit = getPhrase();
-  for (let i = 0; i < phraseSplit.length; i++) {
-    if (phraseSplit[i] == "") {
-      $('#phrase ul').append('<li class="letter"></li>');
-    } else{
-      $('#phrase ul').append('<li class="letter"></li>');
+  for(let i = 0; i < phraseSplit.length; i++) {
+    if (phraseSplit[i] == " ") {
+      $('#phrase ul').append('<li class="space"> </li>');
+    } else {
+      $('#phrase ul').append('<li class="letter ' + phraseSplit[i].toLowerCase() + '">'+ phraseSplit[i] + '</li>');
     }
   }
-})
+  phraseLength = phrase.replace(/\s/g, "").split('').length;
+});
 
+
+// Get a random phrase from phrase list
 let getPhrase = () => {
-  let phrase = phrasesList[Math.floor((Math.random() * 5) + 1)];
-  console.log(Math.floor((Math.random() * 5) + 1));
+  phrase = phrasesList[Math.floor((Math.random() * 4) + 1)];
   let phraseSplit = phrase.split('');
   return phraseSplit;
 }
 
+
 $('#qwerty button').click(function(event) {
-  // alert($(this).text());
-  console.log($(this).text());
+  if(lives == 1) {
+    $('.lose').css('display','flex');
+    restartGame();
+  } else {
+  $(this).attr('disabled', 'true');
+  $(this).addClass('chosen');
+  checkLetter($(this).text());
+  }
 });
+
+
+let checkLetter = (letter) => {   // check to see if letter is in phrase and then reveal letter
+  let splitPhrase = phrase.replace(/ /g,'').split('');
+  let selector = '.' + letter;
+  if($(selector).length != 0) {
+    $(selector).addClass('show');
+  } else {      // subtract a life for incorrect guess
+    let tries = $('.tries img');
+    let arrayNum = lives-1;
+    $(tries[arrayNum]).attr('src','images/lostHeart.png');
+    lives = lives-1;
+    }
+    if(phraseLength == $('.show').length) {
+      $('.win').css('display','flex');
+      restartGame();
+    }
+  }
+
+
+let restartGame = () => {
+  lives = 5
+  $('.tries img').attr('src', 'images/liveHeart.png');
+  $('.letter, .space').remove();
+  $('.chosen').removeClass('chosen');
+  $('#qwerty button').removeAttr('disabled');
+}
